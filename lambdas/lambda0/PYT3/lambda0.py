7bd681f14e535a2fd40cea0651fbbe9ad1908e66
Hongwei <gmhwxi@gmail.com>
2024-09-11 00:14:10 -0400

    Updating: very very minorly

--- lambdas/lambda0/PYT3/lambda0.py
+++ lambdas/lambda0/PYT3/lambda0.py
@@ -9,9 +9,9 @@
 # | TMapp of (term, term)
 ############################################################
 #
-TMvar0 = 0
-TMlam0 = 1
-TMapp0 = 2
+TM0var = 0
+TM0lam = 1
+TM0app = 2
 #
 class term:
     ctag = -1
@@ -22,7 +22,7 @@ class term:
 class term_var(term):
     def __init__(self, arg1):
         self.arg1 = arg1
-        self.ctag = TMvar0
+        self.ctag = TM0var
     def __str__(self):
         return ("TMvar(" + self.arg1 + ")")
 # end-of-class(term_var(term))
@@ -31,7 +31,7 @@ class term_lam(term):
     def __init__(self, arg1, arg2):
         self.arg1 = arg1
         self.arg2 = arg2
-        self.ctag = TMlam0
+        self.ctag = TM0lam
     def __str__(self):
         return ("TMlam(" + self.arg1 + "," + str(self.arg2) + ")")
 # end-of-class(term_lam(term))
@@ -40,37 +40,84 @@ class term_app(term):
     def __init__(self, arg1, arg2):
         self.arg1 = arg1
         self.arg2 = arg2
-        self.ctag = TMapp0
+        self.ctag = TM0app
     def __str__(self):
         return ("TMapp(" + str(self.arg1) + "," + str(self.arg2) + ")")
 # end-of-class(term_app(term))
 #
 ############################################################
 #
-def TMvar1(x00):
+def TM1var(x00):
     return term_var(x00)
-def TMlam1(x00, tm1):
+def TM1lam(x00, tm1):
     return term_lam(x00, tm1)
-def TMapp1(tm1, tm2):
+def TM1app(tm1, tm2):
     return term_app(tm1, tm2)
 #
 ############################################################
 #
-def I():
-    x = TMvar1("x")
-    return TMlam1("x", x)
-def K():
-    x = TMvar1("x")
-    return TMlam1("x", TMlam1("y", x))
-def S():
-    x = TMvar1("x")
-    y = TMvar1("y")
-    z = TMvar1("z")
-    return TMlam1("x", TMlam1("y", TMlam1("z", TMapp1(TMapp1(x, z), TMapp1(y, z)))))
+def I_():
+    x = TM1var("x")
+    return TM1lam("x", x)
+def K_():
+    x = TM1var("x")
+    return TM1lam("x", TM1lam("y", x))
+def S_():
+    x = TM1var("x")
+    y = TM1var("y")
+    z = TM1var("z")
+    return TM1lam("x", TM1lam("y", TM1lam("z", TM1app(TM1app(x, z), TM1app(y, z)))))
 #
-_ = print("I =", I())
-_ = print("K =", K())
-_ = print("S =", S())
+_ = print("I =", I_())
+_ = print("K =", K_())
+_ = print("S =", S_())
+#
+############################################################
+
+def term_subst(tm0, x00, sub):
+    def subst(tm0):
+        return term_subst(tm0, x00, sub)
+    if (tm0.ctag == TM0var):
+        x01 = tm0.arg1
+        return sub if (x00 == x01) else tm0
+    if (tm0.ctag == TM0lam):
+        x01 = tm0.arg1
+        return tm0 if (x00 == x01) else TM1lam(x01, subst(tm0.arg2))
+    if (tm0.ctag == TM0app):
+        return TM1app(subst(tm0.arg1), subst(tm0.arg2))
+    raise TypeError(tm0) # HX: should be deadcode!
+    
+############################################################
+
+def term_beta2red(tm1, tm2):
+    assert tm1.ctag == TM0lam
+    return term_subst(tm1.arg2, tm1.arg1, tm2)
+
+def term_evaluate(tm0):
+    # print("term_evaluate: tm0 = ", tm0)
+    if (tm0.ctag == TM0var):
+        return tm0
+    if (tm0.ctag == TM0lam):
+        return tm0
+    if (tm0.ctag == TM0app):
+        tm1 = tm0.arg1
+        tm2 = tm0.arg2
+        tm1 = term_evaluate(tm1)
+        if (tm1.ctag == TM0lam):
+            return term_evaluate(term_beta2red(tm1, tm2))
+        else:
+            return TM1app(tm1, term_evaluate(tm2))
+    raise TypeError(tm0) # HX: should be deadcode!
+
+############################################################
+#
+def SKKx():
+    S = S_()
+    K = K_()
+    x = TM1var("x")
+    return TM1app(TM1app(TM1app(S, K), K), x)
+#
+print("evaluate(SKKx) =", term_evaluate(SKKx()))
 #
 ############################################################
 ############################################################
